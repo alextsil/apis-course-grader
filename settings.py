27a67eb77d64eb418c0ee722a6c05feb1ce24cb8
Alex Tsilingiris <alex19924@gmail.com>
2018-11-12 11:55:36 +0200

    Added comments on settings file

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 class Settings:
-    ws_conn_string = "http://127.0.0.1:8080"
-    db_conn_string = "mongodb://127.0.0.1:27017"
+    ws_conn_string = "http://127.0.0.1:5110"  # Flask default port = 5110, Spring default port = 8080
+    db_conn_string = "mongodb://127.0.0.1:27017"  # Authorization = disabled, no login required
     db_db = "apis"
     db_collection = "apis"
